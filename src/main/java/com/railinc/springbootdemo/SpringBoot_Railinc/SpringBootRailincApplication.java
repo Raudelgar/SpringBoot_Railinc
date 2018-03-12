7a6ef2274a96b2b1ab4040128645a4ff173a3c26
Raudel Garcia <raudel24@gmail.com>
2018-03-12 17:33:52 -0400

    Initial commit

---
+++ src/main/java/com/railinc/springbootdemo/SpringBoot_Railinc/SpringBootRailincApplication.java
@@ -0,0 +1,12 @@
+package com.railinc.springbootdemo.SpringBoot_Railinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRailincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRailincApplication.class, args);
+	}
+}
